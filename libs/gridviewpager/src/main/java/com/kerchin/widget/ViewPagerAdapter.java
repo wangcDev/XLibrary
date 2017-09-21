58f7d00a2d1e22a27da4c1db486f069dd7ac345a
Wang Cheng <415360804@qq.com>
2017-09-21 10:28:02 +0800

    commit

--- libs/gridviewpager/src/main/java/com/kerchin/widget/ViewPagerAdapter.java
+++ libs/gridviewpager/src/main/java/com/kerchin/widget/ViewPagerAdapter.java
@@ -25,7 +25,6 @@ public class ViewPagerAdapter<T extends View> extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        Log.e("11111111111111","getHeight = "+mViewList.get(position).getHeight()+"getWidth = "+mViewList.get(position).getWidth());
         container.addView(mViewList.get(position));
         return (mViewList.get(position));
     }
